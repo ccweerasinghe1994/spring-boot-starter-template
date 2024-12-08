32e674fde768ab7cf023b18e361e66a94c38f12e
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-12-08 12:01:29 +0530

    Initialize Spring Boot starter template with basic configuration and structure

---
+++ src/test/java/tech/cgnexus/spring_boot_starter_template/SpringBootStarterTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.cgnexus.spring_boot_starter_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
